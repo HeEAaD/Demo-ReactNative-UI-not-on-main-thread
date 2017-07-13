237e44a0cbe1a005aa3f941ba1ba4b245f23a0c1
Steffen Matthischke <steffen.matthischke@lovoo.com>
2017-07-13 22:23:28 +0200

    simplify index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ScrollView,
   StyleSheet,
   Text,
   View
@@ -16,16 +17,11 @@ export default class ReactNativeUINotOnMainThread extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <ScrollView>
+        <Text style={styles.text}>
+          {"ꜛ\nScroll me\nꜜ"}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        </ScrollView>
       </View>
     );
   }
@@ -38,16 +34,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  text: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    marginTop: 200,
+  }
 });
 
 AppRegistry.registerComponent('ReactNativeUINotOnMainThread', () => ReactNativeUINotOnMainThread);
